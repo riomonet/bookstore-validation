949112bb516986f68322c3d3a991c740bdd4a9a1
riomonet <arizablozki@gmail.com>
2023-07-20 01:22:42 -0400

    add validation and testing

---
+++ routes/books.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const Book = require("../models/book");
+const jsonschema = require('jsonschema')
+const bookSchema = require('../schemas/bookschema.json')
+const router = new express.Router();
+const ExpressError = require('../expressError')
+
+
+/** GET / => {books: [book, ...]}  */
+
+router.get("/", async function (req, res, next) {
+  try {
+    const books = await Book.findAll(req.query);
+    return res.json({ books });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /[id]  => {book: book} */
+
+router.get("/:id", async function (req, res, next) {
+  try {
+    const book = await Book.findOne(req.params.id);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST /   bookData => {book: newBook}  */
+
+router.post("/", async function (req, res, next) {
+    try {
+	const result = jsonschema.validate(req.body, bookSchema)
+	if(!result.valid) {
+	    console.log(result)
+	    const errors = result.errors.map(error => error.stack)
+	    const err = new ExpressError(errors,400)
+	    return next(err)
+	}
+	const book = await Book.create(req.body.book);
+	console.log(book)
+	return res.status(201).json({ book });
+
+    } catch (err) {
+	return next(err);
+    }
+});
+
+/** PUT /[isbn]   bookData => {book: updatedBook}  */
+
+router.put("/:isbn", async function (req, res, next) {
+  try {
+    const book = await Book.update(req.params.isbn, req.body);
+    return res.json({ book });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** DELETE /[isbn]   => {message: "Book deleted"} */
+
+router.delete("/:isbn", async function (req, res, next) {
+  try {
+    await Book.remove(req.params.isbn);
+    return res.json({ message: "Book deleted" });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
